bd847243c059c451d5d3b41cf516773e44080f19
Misha <mishalyagin17@gmail.com>
2024-02-17 17:30:41 +0300

    start2

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+import asyncio
+
 from aiogram.filters import CommandStart, Filter
 from aiogram import Bot, types, Dispatcher, F
 from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
+
+
+TOKEN = '6805910622:AAFaoyIFq8QgK8msxdl6mNekSRk1XSoxbCs'
+
+dp = Dispatcher()
+bot = Bot(TOKEN)
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+
+if __name__ == '__main__':
+    print('Bot is running')
+    asyncio.run(main())
+
